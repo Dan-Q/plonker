27b010cf71e1145e514c6864ee93645a5dec2bcb
Dan Q <dan@danq.me>
2017-01-18 18:25:38 +0100

    Expand initial news story

--- plonker.rb
+++ plonker.rb
@@ -13,7 +13,13 @@ end
 
 # Define a list of domains that we respond to:
 DOMAINS = {
-  'isaplonker.uk' => :plonker,
+  'isaplonker.uk' => { code: :plonker, teaser: '{{fullname}} is a plonker', agency: 'Plonkerwatch News' }
+}
+
+# Load a list of gendered names
+GENDERED_NAMES = {
+  0 => File.read('lib/names/male.txt').split(/[\r\n]+/).reject{|n|n.strip == ''},
+  1 => File.read('lib/names/female.txt').split(/[\r\n]+/).reject{|n|n.strip == ''}
 }
 
 # Returns the Host: header from the original request, e.g. "some.body.isaplonker.uk"
@@ -39,7 +45,7 @@ def name
   name_parts = predomain.gsub(/[^\.\-a-z0-9]/, '').split(/\.+/).map(&:capitalize)
   # Handle Irish-style (O'Something) names
   while (index_of_o = name_parts.find_index('O')) && (index_of_o < (name_parts.length - 1))
-    name_parts[index_of_o + 1] = "O'#{name_parts[index_of_o + 1]}"
+    name_parts[index_of_o + 1] = "O&#8217;#{name_parts[index_of_o + 1]}"
     name_parts.delete_at(index_of_o)
   end
   # Handle Mc/Mac names
@@ -63,15 +69,22 @@ end
 # (he, him, his, his), 1 for female pronouns (she, her, her, hers), 2 for gender-neutral
 # pronouns or where the pronoun can't be guessed (they, them, their, theirs).
 def gender
-  2 # TODO: actually determine
+  # Attempt to determine if gender is being specified by the URL
+  # TODO: determine gender by preference
+  # Failing that, use the first name as a clue
+  GENDERED_NAMES.each do |k, v|
+    return k if v.include?(name.first)
+  end
+  # Failing that, use gender-neutral pronouns
+  return 2
 end
 
 # Returns a full set of pronouns and convenience words, based upon the victim's gender
 def pronouns
   [
-    {he_she_they: 'he',   him_her_them: 'him',  his_her_their: 'his',   his_hers_theirs: 'his',    man_woman_person: 'man',    boy_girl_child: 'boy'},
-    {he_she_they: 'she',  him_her_them: 'her',  his_her_their: 'her',   his_hers_theirs: 'hers',   man_woman_person: 'woman',  boy_girl_child: 'girl'},
-    {he_she_they: 'they', him_her_them: 'them', his_her_their: 'their', his_hers_theirs: 'theirs', man_woman_person: 'person', boy_girl_child: 'child'}
+    {he_she_they: 'he',   him_her_them: 'him',  his_her_their: 'his',   his_hers_theirs: 'his',    man_woman_person: 'man',    boy_girl_child: 'boy',   hes_shes_theyre: "he&#8217;s"},
+    {he_she_they: 'she',  him_her_them: 'her',  his_her_their: 'her',   his_hers_theirs: 'hers',   man_woman_person: 'woman',  boy_girl_child: 'girl',  hes_shes_theyre: "shee&#8217;s"},
+    {he_she_they: 'they', him_her_them: 'them', his_her_their: 'their', his_hers_theirs: 'theirs', man_woman_person: 'person', boy_girl_child: 'child', hes_shes_theyre: "they&#8217;re"}
   ][gender]
 end
 
@@ -79,7 +92,7 @@ end
 get '/' do
   @domain, @predomain, @name, @pronouns = domain, predomain, name, pronouns
   if @name.length > 0
-    erb DOMAINS[@domain]
+    erb DOMAINS[@domain][:code]
   else
     erb :setup
   end
