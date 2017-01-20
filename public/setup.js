5137befe47c5a102862e2efa6a060a036c29fd75
Dan Q <dan@danq.me>
2017-01-20 17:54:53 +0100

    Add skeleton setup page

--- public/setup.js
+++ public/setup.js
@@ -1,2 +1,24 @@
 "use strict";
+(function(){
+  function updateUrl(){
+    var name = $('#name').val();
+    name = name.toLowerCase().replace(/['\. ]+/, '.').replace(/[^a-z0-9\-\.]+/, '').replace(/^\.+/, '').replace(/\.+$/, '');
+    var domain = 'isaplonker.uk';
+    var address = (name.length > 0 ? `${name}.${domain}` : '');
+    $('#result').attr('href', `http://${address}`).text(address);
+  }
 
+  jQuery(function($){
+    $('body').addClass('js');
+
+    $('#advanced-button').on('click', function(){
+      $(this).closest('#setup').toggleClass('advanced');
+      return false;
+    });
+
+    $('#name').on('change keyup', updateUrl);
+    updateUrl();
+
+    $('input:first').focus();
+  });  
+})();
